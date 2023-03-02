429727b34fc878447ddb7f4cc4e99f715775b27f
clowncdi <clowncdi85@gmail.com>
2023-03-02 23:40:49 +0900

    feat: logo 타이핑 모션 추가

--- logo.js
+++ logo.js
@@ -49,7 +49,11 @@ const plus = document.getElementById("logoPlus");
 const typo = document.getElementById("logoTypo");
 const plusFont = document.getElementById("plus-font");
 const typoFont = document.getElementById("typo-font");
-const changeLogoTime = 5000;
+const logo = document.querySelector(".gnb-logo");
+const changeLogoTime = 10000;
+const logoText = 'plusTYPO';
+let currentIndex = 0;
+let opacity = 0;
 
 const randomFont = () => {
   const randomIndex = Math.floor(Math.random() * fonts.length);
@@ -62,33 +66,86 @@ const randomFont = () => {
 // }
 
 const randomLetterStyle = () => {
-  fadeInLogo();
+  addLogoText();
   plus.style.fontFamily = randomFont();
   typo.style.fontFamily = randomFont();
-  plusFont.innerHTML = plus.style.fontFamily.replace(/\"/gi, '');
-  typoFont.innerHTML = typo.style.fontFamily.replace(/\"/gi, '');
-  setTimeout(fadeOutLogo, changeLogoTime - 1000);
+  setTimeout(deleteLogoText, changeLogoTime - 2000);
 }
 
-let opacity = 0;
+const addLogoText = () => {
+  const plusLoop = setInterval(() => {
+    plus.innerHTML += logoText[currentIndex];
+    currentIndex++;
+    if (currentIndex === 4) {
+      clearInterval(plusLoop);
+      
+      const typoLoop = setInterval(() => {
+        typo.innerHTML += logoText[currentIndex];
+        currentIndex++;
+        if (currentIndex === 8) {
+          currentIndex = 0;
+          fadeInLogoFont();
+          plusFont.innerHTML = plus.style.fontFamily.replace(/\"/gi, '');
+          typoFont.innerHTML = typo.style.fontFamily.replace(/\"/gi, '');
+          setTimeout(fadeOutLogoFont, changeLogoTime - 2000);
+          clearInterval(typoLoop);
+        }
+      }, 80);
+    }
+  }, 50);
+}
 
-const fadeInLogo = () => {
+const deleteLogoText = () => {
+  const typoLoop = setInterval(() => {
+    typo.innerHTML = typo.innerHTML.slice(0, -1);
+    if (typo.innerHTML.length === 0) {
+      clearInterval(typoLoop);
+      
+      const plusLoop = setInterval(() => {
+        plus.innerHTML = plus.innerHTML.slice(0, -1);
+        if (plus.innerHTML.length === 0) {
+          clearInterval(plusLoop);
+        }
+      }, 80);
+    }
+  }, 50);
+}
+
+// blinking type cursor
+const fontCursor = document.createElement("span");
+fontCursor.innerHTML = "|";
+fontCursor.style.fontFamily = "Pretendard Variable";
+fontCursor.style.fontWeight = 100;
+fontCursor.style.opacity = "0";
+fontCursor.style.color = "black";
+fontCursor.style.fontSize = "3rem";
+fontCursor.style.marginTop = "0.3rem";
+logo.appendChild(fontCursor);
+
+const cursorBlinkTime = 500;
+const cursorBlink = () => {
+  fontCursor.style.opacity = fontCursor.style.opacity === "0" ? "1" : "0";
+}
+setInterval(cursorBlink, cursorBlinkTime);
+
+
+const fadeInLogoFont = () => {
   opacity += 0.1;  
-  plus.style.opacity = opacity;
-  typo.style.opacity = opacity;
+  plusFont.style.opacity = opacity;
+  typoFont.style.opacity = opacity;
   if (opacity < 1) {
-    setTimeout(fadeInLogo, 50);
+    setTimeout(fadeInLogoFont, 50);
   }
 }
 
-const fadeOutLogo = () => {
+const fadeOutLogoFont = () => {
   opacity -= 0.1;  
-  plus.style.opacity = opacity;
-  typo.style.opacity = opacity;
+  plusFont.style.opacity = opacity;
+  typoFont.style.opacity = opacity;
   if (opacity > 0) {
-    setTimeout(fadeOutLogo, 50);
+    setTimeout(fadeOutLogoFont, 50);
   }
-
 }
+
 randomLetterStyle();
 setInterval(randomLetterStyle, changeLogoTime);
