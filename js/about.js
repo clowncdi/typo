a83badbc95313422d782ba946f21ac09531716cd
clowncdi <clowncdi85@gmail.com>
2023-03-18 01:11:53 +0900

    fix: 불필요한 코드 삭제

--- js/about.js
+++ js/about.js
@@ -44,3 +44,9 @@ function setLang(lang) {
     sub3.innerHTML = ENG.sub3;
   }
 }
+
+function isMobile() {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    window.navigator.userAgent
+  );
+}
